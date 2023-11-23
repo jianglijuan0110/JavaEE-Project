2178c1988e6aba422c9f395a07ca106db599d327
jianglijuan0110 <jianglijuan0110@126.com>
2023-11-23 19:03:43 +0100

    desactive clé etrangère

--- src/main/java/repositories/LieuRepository.java
+++ src/main/java/repositories/LieuRepository.java
@@ -1,11 +1,20 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import models.Lieu;
 
 @Repository
 public interface LieuRepository extends JpaRepository<Lieu, String> {
+    @Modifying
+    @Query(value = "SET foreign_key_checks = 0 FOR lieu", nativeQuery = true)
+    void disableForeignKeyChecks();
 
+    @Modifying
+    @Query(value = "SET foreign_key_checks = 1 FOR lieu", nativeQuery = true)
+    void enableForeignKeyChecks();
 }
+
--- src/main/java/services/implementations/LieuImpl.java
+++ src/main/java/services/implementations/LieuImpl.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import models.Lieu;
+import org.springframework.transaction.annotation.Transactional;
 import repositories.LieuRepository;
 import services.LieuService;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 @Service
 public class LieuImpl implements LieuService {
-	
+	@PersistenceContext
+	private EntityManager entityManager;
+
 	@Autowired
 	private LieuRepository lieuRepository;
 
@@ -35,8 +40,34 @@ public class LieuImpl implements LieuService {
 	}
 
 	@Override
+	@Transactional
 	public Lieu saveLieu(Lieu lieu) {
-		return lieuRepository.save(lieu);
+		try {
+			// Disable foreign key checks for Lieu
+			disableForeignKeyChecks("lieu");
+			// Save the Lieu using the repository
+			Lieu savedLieu = lieuRepository.save(lieu);
+
+
+			// Return the saved Lieu
+			return savedLieu;
+
+		} catch (Exception e) {
+
+			throw new RuntimeException("Error saving Lieu: " + e.getMessage(), e);
+		} finally {
+			// Enable foreign key checks for Lieu
+			enableForeignKeyChecks("lieu");
+		}
 	}
 
+	private void disableForeignKeyChecks(String tableName) {
+		String query = String.format("SET foreign_key_checks = 0 FOR %s", tableName);
+		entityManager.createNativeQuery(query).executeUpdate();
+	}
+
+	private void enableForeignKeyChecks(String tableName) {
+		String query = String.format("SET foreign_key_checks = 1 FOR %s", tableName);
+		entityManager.createNativeQuery(query).executeUpdate();
+	}
 }
