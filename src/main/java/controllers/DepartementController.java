61f0d5b2de9f9f3e80f4fb822892b8ea62c53444
jianglijuan0110 <jianglijuan0110@126.com>
2023-11-25 22:50:11 +0100

    departement new ne marche plus

--- src/main/java/controllers/DepartementController.java
+++ src/main/java/controllers/DepartementController.java
@@ -43,14 +43,12 @@ public class DepartementController {
 
 	@GetMapping("/departement/new")
 	public String createDepartement(Model model) {
-		// Create a new Departement instance
+
 		Departement departement = new Departement();
-		// Pre-fill the "dep" field with the value of "insertedDep"
-		departement.setDep(insertedDep); // You should define "insertedDep" somewhere
-		// Add the departement object to the model
 		model.addAttribute("departement", departement);
 		return "Create_Departement";
 	}
+
 
 	@PostMapping("/departement/save")
 	public String saveDepartement(@ModelAttribute("departement") Departement departement,
