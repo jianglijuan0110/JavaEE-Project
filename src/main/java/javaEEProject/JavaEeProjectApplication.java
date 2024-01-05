5e2704e8ceb01e277fe8640bfec74b38603e02af
jianglijuan0110 <35961632+jianglijuan0110@users.noreply.github.com>
2024-01-05 16:48:00 +0100

    security

--- src/main/java/javaEEProject/JavaEeProjectApplication.java
+++ src/main/java/javaEEProject/JavaEeProjectApplication.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 		"repositories"
 })
 @SpringBootApplication(scanBasePackages= {
-		"security",
+		//"security",
 		"controllers",
 		"services",
 		"services.implementations"
